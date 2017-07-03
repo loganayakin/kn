27ca11b0e552f732ea48d0252eb35346731dbf59
loganayakin <logusweety13@gmail.com>
2017-07-03 11:02:05 +0530

    Create even.java

---
+++ even.java
@@ -0,0 +1,13 @@
+	public static void main(String args[])
+	{
+	int m=50;
+  System.out.println(" printing the even numbers 1 and "m + m);
+  
+	for(i=0;i<=m;i++)
+	{
+	if( i% 2==0)
+	{
+	System.out.println(i+"");
+	}
+	}
+  }
